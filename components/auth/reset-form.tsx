dd8e8090e87fe78944f42f2d3f8f06c60b2d59d4
iamroushan <roushanrnd@gmail.com>
2024-04-17 01:32:56 +0530

    Add password reset functionality to auth

--- components/auth/reset-form.tsx
+++ components/auth/reset-form.tsx
@@ -3,7 +3,6 @@ import * as z from "zod"
 import { useState, useTransition } from "react"
 import {useForm} from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { useSearchParams } from "next/navigation"
 
 import {
     Form,
@@ -15,36 +14,33 @@ import {
 } from "@/components/ui/form"
 
 import { CardWrapper } from "@/components/auth/card-wrapper"
-import { LoginSchema } from "@/schemas"
+import { ResetSchema } from "@/schemas"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { FormError } from "@/components/form-error"
 import { FormSuccess } from "@/components/form-success"
-import { login } from "@/actions/login"
-import Link from "next/link"
+import { reset } from "@/actions/reset"
 
-export const LoginForm = () => {
-    const searchParams = useSearchParams()
-    const urlError = searchParams.get("error") === "OAuthAccountNotLinked" ? "Emali already in use with different provider!" : ""
+export const ResetForm = () => {
 
     const [error, setError] = useState<string | undefined>("")
     const [success, setSuccess] = useState<string | undefined>("")
     const [isPending, startTransition] = useTransition()
 
-    const form = useForm<z.infer<typeof LoginSchema>>({
-        resolver: zodResolver(LoginSchema),
+    const form = useForm<z.infer<typeof ResetSchema>>({
+        resolver: zodResolver(ResetSchema),
         defaultValues:{
             email: "",
-            password: ""
         }
     })
 
-    const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+    const onSubmit = (values: z.infer<typeof ResetSchema>) => {
         setError("")
         setSuccess("")
 
+
         startTransition(() => {
-            login(values)  // server actions
+            reset(values)  // server actions
             .then((data) => {
                 setError(data?.error)
                 setSuccess(data?.success)
@@ -54,10 +50,9 @@ export const LoginForm = () => {
     }
     return (
         <CardWrapper
-            headerLabel="Welcome back"
-            backButtonLabel="Don't have an account?"
-            backButtonHref="/auth/register"
-            showSocial
+            headerLabel="Forgot Your Password?"
+            backButtonLabel="Back to login"
+            backButtonHref="/auth/login"
         >
             <Form {...form}>
                 <form 
@@ -86,35 +81,10 @@ export const LoginForm = () => {
                             )}
                         />   
 
-                        <FormField 
-                            control={form.control}
-                            name= "password"
-                            render={({field}) => (
-                                <FormItem>
-                                    <FormLabel>
-                                        Password
-                                    </FormLabel>
-                                    <FormControl>
-                                        <Input
-                                            {...field}
-                                            disabled={isPending}
-                                            placeholder="******"
-                                            type="password"
-                                        />
-                                    </FormControl>
-                                    <Button size="sm" variant="link" asChild className="px-0 font-normal">
-                                        <Link href="/auth/reset">
-                                            Forgot password?
-                                        </Link>
-                                    </Button>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />   
                     </div>
 
                     <FormError 
-                        message={error || urlError}
+                        message={error}
                     />
 
                     <FormSuccess
@@ -126,7 +96,7 @@ export const LoginForm = () => {
                         type="submit"
                         className="w-full"
                     >
-                        Login
+                        Send reset email
                     </Button>
                 </form>
             </Form>
