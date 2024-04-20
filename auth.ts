217286a33363b100ac5e4b1710a797b98ebc31fc
iamroushan <roushanrnd@gmail.com>
2024-04-20 23:22:39 +0530

    Update settings page with new form and validation

--- auth.ts
+++ auth.ts
@@ -6,10 +6,11 @@ import { getUserById } from "@/data/user"
 import { db } from "@/lib/db"
 import {UserRole} from "@prisma/client"
 import { getTwoFactorConfirmationByUserId } from "@/data/two-factor-confirmation"
+import { getAccountByUserId } from "./data/account"
  
 export const { 
     handlers: {GET, POST},
-    auth, signIn, signOut} = NextAuth({
+    auth, signIn, signOut, update} = NextAuth({
     pages:{
         signIn: "/auth/login",
         error: "/auth/error"
@@ -61,6 +62,12 @@ export const {
             if(session.user){
                 session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean
             }
+
+            if(session.user){
+                session.user.name = token.name
+                session.user.email = token.email as string
+                session.user.isOAuth = token.isOAuth as boolean
+            }
             return session
         },
         async jwt ({token}){
@@ -70,6 +77,11 @@ export const {
 
             if(!existingUser) return token
 
+            const existingAccount = await getAccountByUserId(existingUser.id)
+
+            token.isOAuth = !!existingAccount
+            token.name = existingUser.name
+            token.email = existingUser.email
             token.role = existingUser.role
             token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled
             return token
